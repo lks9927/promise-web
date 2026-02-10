1065b2790512f56367820e06b5f9706493cd5e4f
lks9927-5017 <lks9927@gmail.com>
2026-02-10 14:06:29 +0900

    fix: syntax error in admin dashboard

--- promise-web/src/pages/AdminDashboard.jsx
+++ promise-web/src/pages/AdminDashboard.jsx
@@ -845,6 +845,8 @@ function CouponPanel({ coupons, onUpdate }) {
                         </div>
                     </div>
                 </div>
+            )}
+
             {/* Bulk Result Modal */}
             {generatedBatch && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4">
